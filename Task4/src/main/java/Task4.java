45e351c73c10e0a2836034dabb468977bf8a8de3
Nad K <nadinka121@rambler.ru>
2021-12-30 21:43:53 +0300

    added support for negative numbers

--- Task4/src/main/java/Task4.java
+++ Task4/src/main/java/Task4.java
@@ -5,8 +5,8 @@ public class Task4 {
             String stringToParse;
             Integer [] arr;
             stringToParse = scannerString();
-            if (stringToParse.matches("(\\d+(\\ )?)+")) {
-            arr=stringToInt(stringToParse);
+            if (stringToParse.matches("(\\-?\\d+(\\ )?)+")) {
+            arr=stringToInt(parseString(stringToParse));
                     System.out.println("Sum = " + sum(arr));
                     System.out.print("Multiply = " + multiply(arr));
                 }
@@ -20,12 +20,13 @@ public class Task4 {
         stringToParse = in.nextLine();
         return stringToParse;
     }
-    static Integer [] stringToInt (String stringToParse) {
-        String [] tempString;
-        int i = 0;
-        tempString = stringToParse.split(" ");
-        Integer[] intArray = new Integer[tempString.length];
-            for (String iter : tempString) {
+    static String [] parseString (String stringToParse) {
+        return stringToParse.split(" ");
+    }
+    static Integer [] stringToInt (String [] stringParse) {
+        int i=0;
+        Integer[] intArray = new Integer[stringParse.length];
+            for (String iter : stringParse) {
                 intArray[i] = Integer.parseInt(iter);
                 i++;
             }
